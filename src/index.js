e1b0f9b2107632c298dfd7ee0cc57fd4ccb210f9
Urusa <usaiyed.9127@gmail.com>
2021-02-08 18:49:18 -0700

    initial files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Table from './Table';
+//import reportWebVitals from './reportWebVitals';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
